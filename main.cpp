49a2048f0f8db7ec7949aad0be06798619d57e2f
bittiboop <sstrelkova15@gmail.com>
2024-07-03 16:46:18 +0300

    added ex 1

--- main.cpp
+++ main.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
 using namespace std;
 
+struct Rectangle {
+    int x;
+    int y;
+    int width;
+    int height;
+};
+void moveRectangle(Rectangle& rect, int dX, int dY) {
+    rect.x += dX;
+    rect.y += dY;
+}
+
+void resizeRectangle(Rectangle& rect, int newWidth, int newHeight) {
+    rect.width = newWidth;
+    rect.height = newHeight;
+}
+
+void printRectangle(const Rectangle& rect) {
+    cout << "Rectangle (x: " << rect.x << ", y: " << rect.y << ", width: " << rect.width << ", height: " << rect.height << ")" << endl;
+}
+
 int main() {
-	cout << "Hello, World!" << endl;
-	return 0;
+    Rectangle rect = { 10, 20, 30, 40 };
+
+    cout << "Start rect. ";
+    printRectangle(rect);
+
+    moveRectangle(rect, 5, -10);
+    cout << "After moving ";
+    printRectangle(rect);
+
+    resizeRectangle(rect, 50, 60);
+    cout << "After resizing ";
+    printRectangle(rect);
+
+    return 0;
 }
